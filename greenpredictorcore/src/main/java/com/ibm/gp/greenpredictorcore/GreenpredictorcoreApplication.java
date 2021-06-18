61620b009f4a3be52b8dbda51ec1da747cdc6d91
namburimk <namburimk@in.ibm.com>
2021-06-18 13:45:46 +0530

    added core project

---
+++ greenpredictorcore/src/main/java/com/ibm/gp/greenpredictorcore/GreenpredictorcoreApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.gp.greenpredictorcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenpredictorcoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenpredictorcoreApplication.class, args);
+	}
+
+}
---
+++ greenpredictorcore/src/test/java/com/ibm/gp/greenpredictorcore/GreenpredictorcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.gp.greenpredictorcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenpredictorcoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
